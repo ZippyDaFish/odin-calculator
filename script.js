55fbc15693682c2531b735409395b5dd52938b18
ZippyDaFish <andrew.riker21@gmail.com>
2022-07-25 12:31:36 -0400

    Add operate function

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+function operate(op, num1, num2){
+    let result = 0;
+    if(op === 0){
+        result = add(num1,num2);
+    }
+    else if(op === 1){
+        result = subtract(num1,num2);
+    }
+    else if(op === 2){
+        result = multiply(num1,num2);
+    }
+    else if(op === 3){
+        result = divide(num1,num2);
+    }
+    console.log(result);
+}
