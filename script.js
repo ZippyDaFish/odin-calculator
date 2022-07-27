9225b06dac69aa2bcbce006565e030338a1eeec1
ZippyDaFish <andrew.riker21@gmail.com>
2022-07-27 17:56:19 -0400

    Add display updates and input gathering

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var input = []
+
+//basic operation functions
 function add(num1, num2){
     return num1 + num2;
 }
@@ -11,19 +14,48 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
+//decides which operation function to use based on given operator
 function operate(op, num1, num2){
     let result = 0;
-    if(op === 0){
+    if(op === "+"){
         result = add(num1,num2);
     }
-    else if(op === 1){
+    else if(op === "-"){
         result = subtract(num1,num2);
     }
-    else if(op === 2){
+    else if(op === "*"){
         result = multiply(num1,num2);
     }
-    else if(op === 3){
+    else if(op === "/"){
         result = divide(num1,num2);
     }
     console.log(result);
+}
+
+//display updating and input capturing functions
+function addInput(val){
+    input.push(val);
+    console.log(input);
+    updateDisplay();
+}
+function deleteLast(){
+    input.pop();
+    console.log(input);
+    updateDisplay();
+}
+function clearInput(){
+    input = [];
+    console.log(input);
+    updateDisplay();
+}
+function updateDisplay(){
+    toDisplay = input.toString();
+    toDisplay = toDisplay.replaceAll(',','');
+    if(toDisplay){
+        document.getElementById("display").innerText = toDisplay;
+    }
+    else{
+        document.getElementById("display").innerText = "0";
+    }
+
 }
